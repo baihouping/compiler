28d63da64799a16c8d86bf8282927e4ef09aded6
baihouping <844839320@qq.com>
2021-07-23 16:06:07 +0800

    项目初始化

---
+++ Demo01/Demo01/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  Demo01
+//
+//  Created by apple on 2021/7/23.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
